770076bed1b7f25741c4c494d3dd0db1101f3e07
Cooper <55swimmer55@gmail.com>
2023-09-19 11:37:38 -0400

    completed mock challenge minus POST request deliverable

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+
+//const baseURL = "http://localhost:3000/plants"
+
   return (
     <div className="app">
       <Header />
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plantArr, setPlantArr] = useState([])
+
+  const [plantSearch, setPlantSearch] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:3000/plants")
+      .then((response) => response.json())
+      .then((data) => setPlantArr(data))
+  }, [])
+
+  const filteredPlantArr = plantArr.filter((plant) => (
+    plant.name.toLowerCase().includes(plantSearch.toLowerCase())
+  ))
+
   return (
     <main>
       <NewPlantForm />
-      <Search />
-      <PlantList />
+      <Search setPlantSearch={setPlantSearch} />
+      <PlantList plantArr={filteredPlantArr} />
     </main>
   );
 }
